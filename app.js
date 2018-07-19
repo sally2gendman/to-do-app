b2300382d68ae61c7dad30bf6873e6ce24347fa6
Sally Tugendman <sally.tugendman3@gmail.com>
2018-07-18 17:28:21 -0700

    Fixed typo in js

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 function onReady() {
-  const addToDoForm = document.getElementsById('addToDoForm');
+  const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
-  addToDoForm.addEventListener('submit', () => {
+
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
     let title = newToDoText.value;
 
     let newLi = document.createElement('li');
 
-
     let checkbox = document.createElement('input');
-
 
     checkbox.type = "checkbox";
 
-   newLi.textContent = title;
+    newLi.textContent = title;
 
-
-   newLi.appendChild(checkbox);
+    newLi.appendChild(checkbox);
 
     toDoList.appendChild(newLi);
-  
+
     newToDoText.value = '';
   });
 }
+
 window.onload = function() {
    onReady();
 };
