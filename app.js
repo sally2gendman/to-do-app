c5977000bf92e15b2057d4a0c39ad8daab5cc70d
Sally Tugendman <sally.tugendman3@gmail.com>
2018-07-19 17:25:56 -0700

    Checkpoint 6 events part 2: completed

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  function createNewToDo() {
+      const newToDoText = document.getElementById('newToDoText');
+      if (!newToDoText.value) {return; }
+      toDos.push({
+        title: newToDoText.value,
+        complete: false
+      });
+      newToDoText.value = '';
+      renderTheUI();
+  }
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
 
-  addToDoForm.addEventListener('submit', event => {
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+    addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    let title = newToDoText.value;
-
-    let newLi = document.createElement('li');
-
-    let checkbox = document.createElement('input');
-
-    checkbox.type = "checkbox";
-
-    newLi.textContent = title;
-
-    newLi.appendChild(checkbox);
-
-    toDoList.appendChild(newLi);
-
-    newToDoText.value = '';
+    createNewToDo();
   });
+  renderTheUI();
 }
 
 window.onload = function() {
